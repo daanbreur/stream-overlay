18730c440918dfae7ec44b7efc48d220df4863f5
Daan Breur <daanbreur@gmail.com>
2021-06-05 10:07:51 +0200

    :zap: Migrate team command to MongoDB
    Change the team command over to mongodb.

--- commands/team.js
+++ commands/team.js
@@ -1,23 +1,24 @@
-const { err } = require( '../logger' )
+const { err } = require("../logger");
+const User = require("../models/User");
 
-module.exports.run = async (client, wss, message, args, {channel, tags}) => {
-	try {
-		let doc = await find(tags['user-id']);
-
-		if (!(args[0] === 'x' || args[0] === 'o')) {
-			client.say(channel, `@${tags.username}, to join a team type !team x|o`);
-			return;
+module.exports.run = async (client, wss, message, args, { channel, tags }) => {
+	if (args[0] === "red" || args[0] === "blue") {
+		User.findOne({ twitchId: tags["user-id"] })
+			.then(async (document) => {
+				if (document == null) {
+					User.create({ twitchId: tags["user-id"], displayName: tags["display-name"], team: args[0] }).catch((err) => err("Command: Team", err));;
+				} else {
+					document.team = args[0];
+					document.save();
+				}
+			})
+			.catch((err) => err("Command: Team", err));
+	} else {
+		client.say(channel, `@${tags.username}, To join a team. Type !team red or !team blue`);
+		let userInDatabase = await User.exists({ twitchId: tags["user-id"] });
+		if (!userInDatabase) {
+			User.create({ twitchId: tags["user-id"], displayName: tags["display-name"] }).catch((err) => err("Command: Team", err));;
 		}
-
-		if (doc == null) {
-			insert(tags['user-id'], { points: 0, team: args[0] });
-		} else {
-			update(tags['user-id'], 'team', args[0]);
-    }
-    
-    client.say(channel, `@${tags.username}, you joined team ${args[0]}`);
-	} catch (error) {
-		err('Command: team-select', error)
 	}
 };
 
