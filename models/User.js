26aaf9ab05d76a9487e89a4ee9bd8a63eb99ec0a
Daan Breur <daanbreur@gmail.com>
2021-06-05 10:01:39 +0200

    :card_file_box: Create the User model for the Database.
    This commit made the *User.js* file that contains the User Schema and User Model for Mongoose

---
+++ models/User.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  twitchId: {type: String, required: true},
+  displayName: String,
+  team: String,
+  points: {type: Number, default: 0},
+  permissions: {
+    refreshOverlayElements: {type: Boolean, default: false},
+    adminCommands: {type: Boolean, default: false}
+  }
+});
+
+module.exports = mongoose.model('User', userSchema);
