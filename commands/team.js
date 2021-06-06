a0c0d11350458567352d2c589c89e2d00f91ff29
Daan Breur <daanbreur@gmail.com>
2021-06-06 15:56:47 +0200

    :pencil2: Fix typos in *team.js* file

--- commands/team.js
+++ commands/team.js
@@ -11,13 +11,14 @@ module.exports.run = async (client, wss, message, args, { channel, tags }) => {
 					document.team = args[0];
 					document.save();
 				}
+				client.say(channel, `@${tags.username}, You have successfully joined team ${args[0]}`);
 			})
-			.catch((err) => err("Command: Team", err));
+			.catch((err) => err("Command: team", err));
 	} else {
 		client.say(channel, `@${tags.username}, To join a team. Type !team red or !team blue`);
 		let userInDatabase = await User.exists({ twitchId: tags["user-id"] });
 		if (!userInDatabase) {
-			User.create({ twitchId: tags["user-id"], displayName: tags["display-name"] }).catch((err) => err("Command: Team", err));;
+			User.create({ twitchId: tags["user-id"], displayName: tags["display-name"] }).catch((err) => err("Command: team", err));;
 		}
 	}
 };
