0e81f49d899720b53bdec7adecd9926143518467
Aidan James  Bennett <aidanbennett1103@gmail.com>
2021-11-13 13:18:58 +0000

    Add p for allias for points

--- commands/points.js
+++ commands/points.js
@@ -1,20 +1,26 @@
-const { err } = require("../logger");
-const User = require("../models/User");
+const { err } = require('../logger');
+const User = require('../models/User');
 
 module.exports.run = async (client, wss, message, args, { channel, tags }) => {
-	User.findOne({ twitchId: tags["user-id"] })
-		.then(async (document) => {
-			if (document == null) {
-				User.create({ twitchId: tags["user-id"], displayName: tags["display-name"] });
-				client.say(channel, `@${tags.username}, You have 0 points.`);
-			} else {
-				client.say(channel, `@${tags.username}, You have ${document.points} points.`);
-			}
-		})
-		.catch((error) => err("Command: points", error));
+  User.findOne({ twitchId: tags['user-id'] })
+    .then(async (document) => {
+      if (document == null) {
+        User.create({
+          twitchId: tags['user-id'],
+          displayName: tags['display-name'],
+        });
+        client.say(channel, `@${tags.username}, You have 0 points.`);
+      } else {
+        client.say(
+          channel,
+          `@${tags.username}, You have ${document.points} points.`
+        );
+      }
+    })
+    .catch((error) => err('Command: points', error));
 };
 
 module.exports.config = {
   name: 'points',
-  aliases: ['point']
+  aliases: ['point', 'p'],
 };
