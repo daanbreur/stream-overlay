ccc83b500f28c6e6060ad8d7ab6175450ea51801
Aidan James  Bennett <aidanbennett1103@gmail.com>
2021-11-13 13:40:35 +0000

    Add unlurk command

---
+++ commands/unlurk.js
@@ -0,0 +1,29 @@
+const { err } = require('../logger');
+const User = require('../models/User');
+
+module.exports.run = async (client, wss, message, args, { channel, tags }) => {
+  User.findOne({ twitchId: tags['user-id'] })
+    .then(async (document) => {
+      if (document == null) {
+        User.create({
+          twitchId: tags['user-id'],
+          displayName: tags['display-name'],
+        });
+        client.say(
+          channel,
+          `@${tags.username}, welcome back to the stream! We hope your lurk was good!`
+        );
+      } else {
+        client.say(
+          channel,
+          `@${tags.username}, welcome back to the stream! We hope your lurk was good!`
+        );
+      }
+    })
+    .catch((error) => err('Command: lurk', error));
+};
+
+module.exports.config = {
+  name: 'unlurk',
+  aliases: [],
+};
